9097f0c4009cc15876769533b1274e0fc2930e25
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-08 20:31:02 +0200

    Added the neighbours corner cases

--- build/gameOfLife.js
+++ build/gameOfLife.js
@@ -5,26 +5,34 @@ let board = [
   [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0],
 ];
-let boardNextStage = [];
+let boardNextTurn = [];
 
-//Para las celdas en vivas/true/1 
+/*
+[A,b,b,b,B]
+[a,E,E,E,c]
+[a,E,E,E,c]
+[a,E,E,E,c]
+[D,d,d,d,C]
+*/
+
+//Para las celdas vivas/true/1 
 //Si tiene 2 o 3 vecinos vivos sigue vivo, sino muere
 let counterForAlives = 0;
 if (counterForAlives < 2 || counterForAlives > 3){
- boardNextStage[x][y] = 0;
+ boardNextTurn[x][y] = 0;
 }
 else if (counterForAlives === 2 || counterForAlives === 3){
-boardNextStage[x][y] = 1;
+boardNextTurn[x][y] = 1;
 }
 
-//Para las celdas en muertas/false/0
+//Para las celdas muertas/false/0
 // Si tiene 3 vecinos vivos revive
 let counterForDeaths = 0;
 if( counterForDeaths === 3){
-boardNextStage[x][y] = 1;
+boardNextTurn[x][y] = 1;
 }
 
-//Caso del centro de la matriz: 8 vecinos.
+//Caso del centro de la matriz: 8 vecinos (E)
 for (x === 1; x < board.length - 1; x++ ){
   for(y === 1; y < board.length - 1; y++ ){
 
@@ -36,7 +44,111 @@ if(x === 0 && y === 0){}//A
 else if(x === board.length - 1, y === 0){}//B
 else if(x === board.length - 1, y === board.length - 1){}//C
 else if(x === 0, y === board.length - 1)//D
+//Buscador de vecinos.
+//Caso A:
+if(board[0][0] === 0){
+counterForDeaths = 0;
+if(board[x][y + 1] === 1){
+counterForDeaths++;
+}
+else if(board[x + 1][y] === 1){
+counterForDeaths++;
+}
+else if(board[x + 1][y + 1] === 1){
+counterForDeaths++;
+}
+}
 
+if(board[0][0] === 1){
+counterForAlives = 0;
+if(board[x][y + 1] === 1){
+counterForAlives++;
+}
+else if(board[x + 1][y] === 1){
+counterForAlives++;
+}
+else if(board[x + 1][y + 1] === 1){
+counterForAlives++;
+}
+}
+//Caso B:
+if(board[board.length - 1][0] === 0){
+counterForDeaths = 0;
+if(board[x - 1][y] === 1){
+counterForDeaths++;
+}
+else if(board[x][y + 1] === 1){
+counterForDeaths++;
+}
+else if(board[x - 1][y + 1] === 1){
+counterForDeaths++;
+}
+}
+
+if(board[board.length - 1][0] === 1){
+counterForAlives = 0;
+if(board[x - 1][y] === 1){
+counterForAlives++;
+}
+else if(board[x][y + 1] === 1){
+counterForAlives++;
+}
+else if(board[x - 1][y + 1] === 1){
+counterForAlives++;
+}
+}
+//Caso C
+if(board[board.length - 1][board.length - 1] === 0){
+counterForDeaths = 0;
+if(board[x - 1][y] === 1){
+counterForDeaths++;
+}
+else if(board[x][y - 1] === 1){
+counterForDeaths++;
+}
+else if(board[x - 1][y - 1] === 1){
+counterForDeaths++;
+}
+}
+
+if(board[board.length - 1][board.length - 1] === 1){
+counterForAlives = 0;
+if(board[x - 1][y] === 1){
+counterForAlives++;
+}
+else if(board[x][y - 1] === 1){
+counterForAlives++;
+}
+else if(board[x - 1][y - 1] === 1){
+counterForAlives++;
+}
+}
+//Caso D
+if(board[0][board.length - 1] === 0){
+counterForDeaths = 0;
+if(board[x][y - 1] === 1){
+counterForDeaths++;
+}
+else if(board[x + 1][y] === 1){
+counterForDeaths++;
+}
+else if(board[x + 1][y - 1] === 1){
+counterForDeaths++;
+}
+}
+
+if(board[0][board.length - 1] === 1){
+counterForAlives = 0;
+if(board[x][y - 1] === 1){
+counterForAlives++;
+}
+else if(board[x + 1][y] === 1){
+counterForAlives++;
+}
+else if(board[x + 1][y - 1] === 1){
+counterForAlives++;
+}
+}
 //Caso de los 4 bloques laterales : 5 veccinos.
 if(x === 0){
   for(y === 1; y < board.length - 1, y++){}
