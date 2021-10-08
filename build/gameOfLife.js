cb92dfc8227d6c5183ab5a46d3d529614b6fe582
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-08 19:05:17 +0200

    Added the array reading loop

---
+++ build/gameOfLife.js
@@ -0,0 +1,34 @@
+let board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+//Caso del centro de la matriz: 8 vecinos.
+for (x === 1; x < board.length - 1; x++ ){
+  for(y === 1; y < board.length - 1; y++ ){
+
+  }
+}
+
+//Caso de las 4 esquinas: 3 vecinos.
+if(x === 0 && y === 0){}//A
+else if(x === board.length - 1, y === 0){}//B
+else if(x === board.length - 1, y === board.length - 1){}//C
+else if(x === 0, y === board.length - 1)//D
+
+//Caso de los 4 bloques laterales : 5 veccinos.
+if(x === 0){
+  for(y === 1; y < board.length - 1, y++){}
+}//a
+if(y === 0){
+  for(x === 1; x < board.length - 1, x++){}
+}//b
+if(x === board.length - 1){
+  for(y === 1; y < board.length - 1, y++){}
+}//c
+if(y === board.length - 1){
+  for(x === 1; x < board.length - 1, x++){}
+}//d
