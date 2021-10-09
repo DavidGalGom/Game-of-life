cb4ccf2fd68ecc4fba2f3bdd8044440a78521b8e
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-09 20:42:25 +0200

    Add test for every function option

--- src/gameOfLife.test.js
+++ src/gameOfLife.test.js
@@ -91,4 +91,117 @@ describe("function walkOverTheBoard go over the board, and call other functions 
       expect(result).toEqual(expected);
     });
   });
+  describe("function bottomBorder evaluate cells at bottom of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+  describe("function topBorder evaluate cells at top of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],] is introduced, expected a [[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("function leftBorder evaluate cells at left of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[0,0,0,0,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,0,0],[1,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("function rightBorder evaluate cells at right of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,1,1],[0,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("function centerBoard evaluate cells at center of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,0,0,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
 });
