c3f20a324b0e20ab080a2af76e812c97df02765b
David Galvez <davidgalvezgomez01@gmail.com>
2021-10-09 19:55:07 +0200

    Added test for all  corner situations

--- src/gameOfLife.test.js
+++ src/gameOfLife.test.js
@@ -2,7 +2,7 @@ const { walkOverTheBoard } = require("../build/gameOfLife");
 
 describe("function walkOverTheBoard go over the board, and call other functions to solve the value of each cell", () => {
   describe("function upperLeftCorner evaluate the [0][0] position of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
-    test("If a [[0,1,0,0,0],[1,1,0,0,0][0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],] is introduced, expected a [[1,1,0,0,0],[1,1,0,0,0][0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+    test("If a [[0,1,0,0,0],[1,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],] is introduced, expected a [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
       const board = [
         [0, 1, 0, 0, 0],
         [1, 1, 0, 0, 0],
@@ -16,6 +16,74 @@ describe("function walkOverTheBoard go over the board, and call other functions 
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("function upperRightCorner evaluate the [0][board.length - 1] position of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],] is introduced, expected a [[0,0,0,1,1],[0,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("function lowerRightCorner evaluate the [board.length - 1][board.length - 1] position of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,1],[0,0,0,1,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      const result = walkOverTheBoard(board);
+
+      expect(result).toEqual(expected);
+    });
+  });
+  describe("function lowerLeftCorner evaluate the [board.length - 1][0] position of the board, and return 0 or 1 depending on the neighbours alive cells", () => {
+    test("If a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,0,0,0],[0,1,0,0,0],] is introduced, expected a [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,0,0,0],[1,1,0,0,0],]", () => {
+      const board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+      ];
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0],
       ];
 
       const result = walkOverTheBoard(board);
